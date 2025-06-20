d160774395f216001317d1ea3ef2045f90682caa
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2025-06-20 16:48:42 +0200

    feat(validation): detect codeowners file on conventional paths

---
+++ crates/canopus/src/features/validation.rs
@@ -0,0 +1,42 @@
+// Copyright 2025 Dotanuki Labs
+// SPDX-License-Identifier: MIT
+
+use crate::features;
+use std::path::PathBuf;
+
+pub fn validate_codeowners(project_location: &PathBuf) -> anyhow::Result<()> {
+    let codeowners = features::check_conventional_codeowners_location(project_location)?;
+    log::info!("Codeowners file found at: {}", codeowners.to_string_lossy());
+    Ok(())
+}
+
+pub fn check_conventional_codeowners_location(project_location: &PathBuf) -> anyhow::Result<PathBuf> {
+    println!("Project location : {:?}", project_location);
+
+    let possible_locations = [
+        project_location.join(".github/CODEOWNERS"),
+        project_location.join("CODEOWNERS"),
+        project_location.join("docs/CODEOWNERS"),
+    ];
+
+    let config_files = possible_locations
+        .iter()
+        .filter(|path| path.exists())
+        .collect::<Vec<_>>();
+
+    println!("Codeowners files : {:?}", config_files);
+
+    if config_files.is_empty() {
+        anyhow::bail!("No CODEOWNERS definition found in the project");
+    }
+
+    if config_files.len() > 1 {
+        anyhow::bail!("Found multiple definitions for CODEOWNERS");
+    }
+
+    let codeowners = config_files
+        .first()
+        .unwrap_or_else(|| panic!("FATAL: found the CODEOWNERS file cannot construct a path to it"));
+
+    Ok(codeowners.to_path_buf())
+}
