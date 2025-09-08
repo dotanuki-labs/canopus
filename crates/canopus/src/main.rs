d6f539a32ff588decb5c5e49107a97bedb0c1f11
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2025-09-08 15:25:40 +0200

    feat(validation): switch to octocrab github client

--- crates/canopus/src/main.rs
+++ crates/canopus/src/main.rs
@@ -5,30 +5,24 @@ use crate::canopus::Canopus;
 use crate::canopus::validation::CodeOwnersValidator;
 use crate::infra::github::GithubConsistencyChecker;
 use crate::infra::{cli, paths};
-use octorust::Client;
-use octorust::auth::Credentials;
-use policies::ExponentialBackoff;
-use reqwest_retry::policies;
+use octocrab::service::middleware::retry::RetryConfig;
 
 mod canopus;
 mod core;
 mod infra;
 
 fn create_canopus() -> anyhow::Result<Canopus> {
-    // Configuration for the underlying HTTP client
     let max_retries_per_request = 3;
-    let base_http_client = reqwest::Client::builder().build()?;
 
-    let exponential_backoff = ExponentialBackoff::builder().build_with_max_retries(max_retries_per_request);
-    let retry_middleware = reqwest_retry::RetryTransientMiddleware::new_with_policy(exponential_backoff);
-    let custom_http_client = reqwest_middleware::ClientBuilder::new(base_http_client)
-        .with(retry_middleware)
-        .build();
+    let github_pat = std::env::var("GITHUB_TOKEN").unwrap_or("".to_string());
+    let user_agent = format!("{}/{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
 
     // Configuration for the Github Client
-    let user_agent = format!("{}/{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
-    let credentials = std::env::var("GITHUB_TOKEN").map(Credentials::Token).ok();
-    let github_client = Client::custom(user_agent, credentials, custom_http_client);
+    let github_client = octocrab::OctocrabBuilder::new()
+        .personal_token(github_pat)
+        .add_retry_config(RetryConfig::Simple(max_retries_per_request))
+        .add_header(http::header::USER_AGENT, user_agent)
+        .build()?;
     let consistency_checker = GithubConsistencyChecker::ApiBased(github_client);
 
     let path_walker = paths::PathWalker::GitAware;
